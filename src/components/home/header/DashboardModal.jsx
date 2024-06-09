495ee92e56e5df801f310661b52065298deb962b
JeromeStephanie <jeromestephanie0@gmail.com>
2024-06-10 00:21:39 +0200

    Update

--- src/components/home/header/DashboardModal.jsx
+++ src/components/home/header/DashboardModal.jsx
@@ -36,7 +36,7 @@ const DashboardModal = ({ isOpen, onClose }) => {
             <NavLink to="/aboutus" className="cursor-pointer text-[24px] p-[10px]" onClick={() => handleLinkClick("aboutus")}>
               About
             </NavLink>
-            <NavLink to="/booking" className="cursor-pointer text-[24px] p-[10px]" onClick={() => handleLinkClick("booking")}>
+            <NavLink to="/travel" className="cursor-pointer text-[24px] p-[10px]" onClick={() => handleLinkClick("travel")}>
               Booking
             </NavLink>
             <NavLink to="/faq" className="cursor-pointer text-[24px] p-[10px]" onClick={() => handleLinkClick("faq")}>
