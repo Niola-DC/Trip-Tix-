8bac85af1d5828755368146632fe3ecfedbf6ff5
Eniola Fanegan <eniolachinemerem74@gmail.com>
2024-07-22 14:35:35 -0500

    Update data.js

--- src/components/aboutUs/faqs/data.js
+++ src/components/aboutUs/faqs/data.js
@@ -1,19 +1,19 @@
 let data = [
     {
-        question: 'Lorem Ipsum bis simply dummy text of the prin....?',
-        answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        question: 'How do I book a trip?',
+        answer: "To book a trip, open the app and select the 'Book a Trip' option. Enter your pick-up and drop-off locations, select the date and time, choose your preferred vehicle, and confirm your booking. You will receive a confirmation notification once the booking is successful.",
     },
     {
-        question: 'Lorem Ipsum is simply dummy text of the prin....?',
-        answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        question: 'What payment methods are accepted?',
+        answer: "Our app accepts various payment methods, including credit/debit cards, and in-app payments. You can add or update your payment method in the 'Payment' section of the app.",
     },
     {
-        question: 'Lorem Ipsum is simply dummy text of the prin....?',
-        answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        question: 'How do I contact customer support?',
+        answer: "You can contact customer support through the 'Contact' section in the app. Fill out the form with your details and message. You can also reach us via email at support@triptix.com or call our support hotline at +2348024143594.",
     },
     {
-        question: 'Lorem Ipsum is simply dummy text of the prin....?',
-        answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        question: 'How can I view my trip history?',
+        answer: "You can view your trip history by going to the 'View Trips' section in the app. Here, you will find a list of all your past trips along with details such as date, time, route, and fare.",
     },
 ];
 
